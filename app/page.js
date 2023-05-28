1495a0a9d08284030c1a362b6941f5099f441be1
Ogulcan <eskimasal@outlook.com>
2023-05-28 13:40:43 +0300

    first commit

---
+++ app/page.js
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main className=" bg-blue-900 w-screen h-screen flex items-center">
+      <div className="bg-green-300 text-center w-full">
+        <button className="bg-white p-2 px-4 rounded-lg">
+          Login with Google
+        </button>
+      </div>
+    </main>
+  );
+}
