8dad4b70707f003b86d03f93d6475afc5ad8fbb2
Ogulcan <eskimasal@outlook.com>
2023-05-28 18:56:25 +0300

    google auth

--- app/page.js
+++ app/page.js
@@ -1,10 +1,37 @@
+"use client";
+
+import { useSession, signIn, signOut } from "next-auth/react";
+
 export default function Home() {
+  const { data: session } = useSession();
+
+  if (!session) {
+    return (
+      <main className=" bg-blue-900 w-screen h-screen flex items-center">
+        <div className="text-center w-full">
+          <button
+            onClick={() => signIn("google")}
+            className="bg-white p-2 px-4 rounded-lg"
+          >
+            Login with Google
+          </button>
+        </div>
+      </main>
+    );
+  }
+
   return (
     <main className=" bg-blue-900 w-screen h-screen flex items-center">
-      <div className="bg-green-300 text-center w-full">
-        <button className="bg-white p-2 px-4 rounded-lg">
-          Login with Google
-        </button>
+      <div className="text-center w-full">
+        <>
+          Signed in as {session.user.email} <br />
+          <button
+            onClick={() => signOut()}
+            className="bg-white p-2 px-4 rounded-lg"
+          >
+            Sign out
+          </button>
+        </>
       </div>
     </main>
   );
